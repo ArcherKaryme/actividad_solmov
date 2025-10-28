d35c4e36254987bf478553997487cd033b641666
𝐚𝐫𝐭(𝐮𝐫𝐨) <128660664+ArcherKaryme@users.noreply.github.com>
2025-10-27 23:48:25 -0600

    Update database connection parameters in db.php

--- api/db.php
+++ api/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "127.0.0.1";
-$username = "admin";
-$password = "Karyme09";
-$dbname = "alumnosdb";
+$servername = "localhost";
+$username = "zdhwwgju_user1";
+$password = "EvmgRFZkqtbjdFmNzTYh";
+$dbname = "zdhwwgju_aplicacion";
 
 $conn = new mysqli($servername,$username,$password,$dbname);
 
@@ -11,3 +11,4 @@ if($conn->connect_error){
     die("Conexion Fallida".$conn->connect_error);
 
 }
+
