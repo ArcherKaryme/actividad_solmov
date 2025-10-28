8b5ba9dabb72b6b4588dcc9ed83cc5b6bd11d79d
𝐚𝐫𝐭(𝐮𝐫𝐨) <128660664+ArcherKaryme@users.noreply.github.com>
2025-10-27 23:28:48 -0600

    Update database connection credentials in db.php

--- api/db.php
+++ api/db.php
@@ -1,12 +1,13 @@
 <?php
 
 $servername = "127.0.0.1";
-$username = "userphp";
-$password = "12345";
+$username = "admin";
+$password = "Karyme09";
 $dbname = "alumnosdb";
 
 $conn = new mysqli($servername,$username,$password,$dbname);
 
 if($conn->connect_error){
     die("Conexion Fallida".$conn->connect_error);
+
 }
